bc7a31ca5c532385c471623c5b22c62e84ecb088
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 22:07:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ from PIL import Image
 import numpy as np
 
 uploaded_file=st.file_uploader("ファイルアップロード", type='png')
-img=Image.open(uploaded_file)
+image=Image.open(uploaded_file)
+img_array = np.array(image)
+st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
