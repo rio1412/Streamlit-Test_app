9a8a479a5bba8d9ad5ae240567090c8c3da6267c
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 21:21:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+uploaded_file = st.file_uploader("ファイルをアップロードしてください。")
+
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    img_array = np.array(image)
+    st.image(
+        image, caption='upload images',
+        use_column_width=True
+    )
