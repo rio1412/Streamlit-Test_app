d1406f89e7b87c1d8da1c0b300cccb4a1442ebfa
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 21:45:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,55 @@
-import streamlit as st
-import pandas as pd
+#@title **セットアップ**
 
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-if uploaded_file is not None:
+# Clone CodeFormer and enter the CodeFormer folder
+%cd /content
+!rm -rf CodeFormer
+!git clone https://github.com/cedro3/CodeFormer.git
+%cd CodeFormer
 
-    df_tmp = pd.read_csv(
-        copy.deepcopy(uploaded_file),
-        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-        engine='python',
-        na_values='-',
-        header=None)
-    
-    st.markdown('### アクセスログ（先頭5件）')
-    st.write(df_tmp.head(5))
+# Set up the environment
+# Install python dependencies
+!pip install -r requirements.txt
+# Install basicsr
+!python basicsr/setup.py develop
+
+# Download the pre-trained model
+!python scripts/download_pretrained_models.py facelib
+!python scripts/download_pretrained_models.py CodeFormer
+
+# Visualization function
+import cv2
+import matplotlib.pyplot as plt
+def display(img1, img2):
+  fig = plt.figure(figsize=(25, 10))
+  ax1 = fig.add_subplot(1, 2, 1) 
+  plt.title('Input', fontsize=16)
+  ax1.axis('off')
+  ax2 = fig.add_subplot(1, 2, 2)
+  plt.title('CodeFormer', fontsize=16)
+  ax2.axis('off')
+  ax1.imshow(img1)
+  ax2.imshow(img2)
+def imread(img_path):
+  img = cv2.imread(img_path)
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  return img
+
+# display result
+from IPython.display import clear_output
+import os
+import glob
+def display_result(input_folder, result_folder):
+  input_list = sorted(glob.glob(os.path.join(input_folder, '*')))
+  for input_path in input_list:
+    img_input = imread(input_path)
+    basename = os.path.splitext(os.path.basename(input_path))[0]
+    output_path = os.path.join(result_folder, basename+'.png')
+    img_output = imread(output_path) 
+    display(img_input, img_output)
+
+# reset_folder
+import shutil
+def reset_folder(path):
+    if os.path.isdir(path):
+      shutil.rmtree(path)
+    os.makedirs(path,exist_ok=True)
