16509cf69e88baada73747d3da5ac2fb8b231bfe
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 21:28:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,11 @@
-from PIL import Image
-
 import streamlit as st
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
 
-import io 
-uploaded_file = st.file_uploader('Choose a image file')
-
 import pandas as pd
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    img_array = np.array(image)
-    st.image(
-        image, caption='upload images',
-        use_column_width=True
-    )
+    df = pd.read_csv(
+        uploaded_file,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None)
