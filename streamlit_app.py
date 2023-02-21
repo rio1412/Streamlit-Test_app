3cae25523b174a27ffdb028b490561bfa5346137
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 22:26:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,13 @@ import numpy as np
 import cv2
 
 uploaded_file=st.file_uploader("ファイルアップロード", type='png')
-image=Image.open(uploaded_file)
-img_array = np.array(image)
-st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
+img=Image.open(uploaded_file)
+
+#画像の解像度を上げる
+scale_percent = 200 # 200%
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+# resize image
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_LINEAR)
