febd7581ee6f3fed1c5f931597cdaa5851ebc60f
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 22:26:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,7 @@ dim = (width, height)
 
 # resize image
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_LINEAR)
+
+
+img_array = np.array(resized)
+st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
