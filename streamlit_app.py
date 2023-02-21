6d31c733de39e1934425fdabb564a006572f702b
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-21 21:42:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,4 @@
-import copy
-import pytz
-import woothee
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import *
-from pandas import DataFrame
-from datetime import datetime
-import re
-import json
-import pycountry
-from urllib.request import urlopen
-import ipaddress
-from ipaddress import ip_address
-
-st.set_page_config(page_title="main", page_icon='1.jpg')
-st.title("Multiple OSS Access Log Analyzer")
 
 
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
@@ -28,11 +10,6 @@ if uploaded_file is not None:
         engine='python',
         na_values='-',
         header=None)
-
-    default_usecols = []
-    default_names = []
-
-    define_usecols(df_tmp, default_usecols, default_names)
-
+    
     st.markdown('### アクセスログ（先頭5件）')
     st.write(df_tmp.head(5))
