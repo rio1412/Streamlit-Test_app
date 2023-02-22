da62ccd62e5339def01fa404f892cc4f0d6b45a5
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-23 08:23:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,59 +7,44 @@ from tensorflow.keras.models import load_model
 
 st.title('High-Quality Image Super-Resolution')
 
-uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
 
-def build_generator(uploaded_file):
-    input_shape = (96, 96, 3)
-    inputs = Input(input_shape)
-    conv1 = Conv2D(64, 9, padding='same', activation='relu')(inputs)
-    conv2 = Conv2D(64, 3, strides=2, padding='same', activation='relu')(conv1)
-    conv3 = Conv2D(128, 3, padding='same', activation='relu')(conv2)
-    conv4 = Conv2D(128, 3, strides=2, padding='same', activation='relu')(conv3)
-    conv5 = Conv2D(256, 3, padding='same', activation='relu')(conv4)
-    conv6 = Conv2D(256, 3, strides=2, padding='same', activation='relu')(conv5)
-    conv7 = Conv2D(512, 3, padding='same', activation='relu')(conv6)
-    conv8 = Conv2D(512, 3, padding='same', activation='relu')(conv7)
-    up1 = Conv2DTranspose(256, 3, strides=2, padding='same', activation='relu')(conv8)
-    up2 = Conv2DTranspose(128, 3, strides=2, padding='same', activation='relu')(up1)
-    conv9 = Conv2D(3, 9, padding='same', activation='tanh')(up2)
-    outputs = (conv9 + 1) * 127.5
-    return Model(inputs, outputs)
+# モデルを読み込む
+model = tf.keras.models.load_model('model.h5')
 
-def build_discriminator():
-    input_shape = (384, 384, 3)
-    inputs = Input(input_shape)
-    conv1 = Conv2D(64, 3, padding='same', activation='relu')(inputs)
-    conv2 = Conv2D(64, 3, strides=2, padding='same', activation='relu')(conv1)
-    conv3 = Conv2D(128, 3, padding='same', activation='relu')(conv2)
-    conv4 = Conv2D(128, 3, strides=2, padding='same', activation='relu')(conv3)
-    conv5 = Conv2D(256, 3, padding='same', activation='relu')(conv4)
-    conv6 = Conv2D(256, 3, strides=2, padding='same', activation='relu')(conv5)
-    conv7 = Conv2D(512, 3, padding='same', activation='relu')(conv6)
-    conv8 = Conv2D    
-    
-    # ここに高画質化処理のコードを記述します
-def build_srgan():
-    """Builds the SRGAN model."""
-    srgan = load_model()
-    return srgan
+# 画像を高画質化する関数を定義する
+def enhance_image(image):
+    # 画像を読み込む
+    img = Image.open(image).convert('RGB')
+    # 画像をリサイズする
+    img = img.resize((256, 256))
+    # 画像をTensorに変換する
+    x = tf.keras.preprocessing.image.img_to_array(img)
+    x = tf.expand_dims(x, axis=0)
+    # 画像を高画質化する
+    enhanced = model.predict(x)
+    # 画像をPIL Imageに変換する
+    enhanced = tf.squeeze(enhanced, axis=0)
+    enhanced = tf.keras.preprocessing.image.array_to_img(enhanced)
+    # 画像を保存する
+    enhanced.save('enhanced_image.jpg')
+    return enhanced
 
-def upscale_image(image):
-    """Upscales the image using the SRGAN model."""
-    srgan = build_srgan()
-    lr_shape = (image.shape[1], image.shape[0])
-    sr_shape = (lr_shape[0] * 4, lr_shape[1] * 4)
-    lr_image = cv2.resize(image, lr_shape, interpolation=cv2.INTER_CUBIC)
-    lr_image = np.expand_dims(lr_image, axis=0)
-    sr_image = srgan.predict(lr_image)
-    sr_image = np.squeeze(sr_image, axis=0)
-    sr_image = np.clip(sr_image, 0, 255).astype(np.uint8)
-    sr_image = cv2.resize(sr_image, sr_shape, interpolation=cv2.INTER_CUBIC)
-    return sr_image
-
+# Streamlitのインターフェースを作成する
+st.title('画像を高画質化するアプリ')
+uploaded_file = st.file_uploader('画像をアップロードしてください', type=['jpg', 'jpeg', 'png'])
 if uploaded_file is not None:
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-    st.image(image, caption='Original Image', use_column_width=True)
-    
-    high_res_image = upscale_image(image)
-    st.image(high_res_image, caption='High-Quality Image', use_column_width=True)
+    # アップロードされた画像を表示する
+    st.image(uploaded_file, caption='アップロードされた画像', use_column_width=True)
+    # 画像を高画質化するボタンを作成する
+    if st.button('画像を高画質化する'):
+        # プログレスバーを表示する
+        progress_bar = st.progress(0)
+        for percent_complete in range(100):
+            progress_bar.progress(percent_complete + 1)
+        # 画像を高画質化する
+        enhanced_image = enhance_image(uploaded_file)
+        # 画像を表示する
+        st.image(enhanced_image, caption='高画質化された画像', use_column_width=True)
