e243dc4f34ee7ba195c77aaeac0c801568578a55
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-22 23:11:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,31 @@ if uploaded_file is not None:
     
     st.image(high_res_image, caption='High-Quality Image', use_column_width=True)
 
+import tensorflow as tf
+from tensorflow.keras.applications.vgg19 import preprocess_input
+from tensorflow.keras.models import load_model
+
+def build_srgan():
+    """Builds the SRGAN model."""
+    srgan = load_model('srgan.h5')
+    return srgan
+
+def upscale_image(image):
+    """Upscales the image using the SRGAN model."""
+    srgan = build_srgan()
+    lr_shape = (image.shape[1], image.shape[0])
+    sr_shape = (lr_shape[0] * 4, lr_shape[1] * 4)
+    lr_image = cv2.resize(image, lr_shape, interpolation=cv2.INTER_CUBIC)
+    lr_image = np.expand_dims(lr_image, axis=0)
+    sr_image = srgan.predict(lr_image)
+    sr_image = np.squeeze(sr_image, axis=0)
+    sr_image = np.clip(sr_image, 0, 255).astype(np.uint8)
+    sr_image = cv2.resize(sr_image, sr_shape, interpolation=cv2.INTER_CUBIC)
+    return sr_image
+
+if uploaded_file is not None:
+    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    st.image(image, caption='Original Image', use_column_width=True)
+    
+    high_res_image = upscale_image(image)
+    st.image(high_res_image, caption='High-Quality Image', use_column_width=True)
