c47b931fc867f14ffb28bf9734899e56d33ffee9
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-23 08:46:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 
 # モデルを読み込む
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('RCAN_TrainCode/experiment/model/Network_RCAN_BIX2_G10R20P48-2018-07-15-20-14-55.txt')
 
 # 画像を高画質化する関数を定義する
 def enhance_image(image):
