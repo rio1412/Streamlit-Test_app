c7eede69792e3384221abf1eed8739631b4a805f
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-22 23:45:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,6 @@ from tensorflow.keras.models import load_model
 st.title('High-Quality Image Super-Resolution')
 
 uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
-
-if uploaded_file is not None:
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-    st.image(image, caption='Original Image', use_column_width=True)
     
     # ここに高画質化処理のコードを記述します
 
