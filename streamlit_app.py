a2680f5f4aebd773953d5055e76098187f1ab1e9
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-22 22:38:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,26 @@ resized = cv2.resize(image, dim, interpolation = cv2.INTER_LINEAR)
 
 img_array1 = np.array(resized)
 st.image(img_array1,caption = 'サムネイル画像',use_column_width = True)
+
+#@title **画像全体オリジナル**
+import os
+import shutil
+from PIL import Image
+
+pic ='images.jpg'#@param {type:"string"} 
+input_folder = 'inputs/whole_imgs_original'
+reset_folder(input_folder)
+im = icon.open('/content/drive/MyDrive/pic/'+pic)
+im.save(input_folder+'/'+os.path.splitext(pic)[0]+'.png')
+
+w = 0.6 #@param {type:"slider", min:0.1, max:0.9, step:0.1}
+
+
+! python inference_codeformer.py --w $w\
+                                  --test_path $input_folder\
+                                  --bg_upsampler realesrgan\
+                                  --face_upsample
+
+clear_output()
+result_folder = 'results/whole_imgs_original_'+str(w)+'/final_results'
+display_result(input_folder, result_folder )
