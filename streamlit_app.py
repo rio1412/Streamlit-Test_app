f1c5a9a9f6d9b9fb9dd171f40e90283bbf5df442
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-22 23:10:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,15 @@
-#ライブラリのインポート
 import streamlit as st
-from PIL import Image
-import numpy as np
 import cv2
 
-uploaded_file=st.file_uploader("ファイルアップロード", type='png')
-img=Image.open(uploaded_file)
-img_array = np.array(img)
-st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
+st.title('High-Quality Image Super-Resolution')
 
+uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
 
-image = cv2.imread("icon.png")
+if uploaded_file is not None:
+    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    st.image(image, caption='Original Image', use_column_width=True)
+    
+    # ここに高画質化処理のコードを記述します
+    
+    st.image(high_res_image, caption='High-Quality Image', use_column_width=True)
 
-#画像の解像度を上げる
-scale_percent = 200 # 200%
-width = int(image.shape[1] * scale_percent / 100)
-height = int(image.shape[0] * scale_percent / 100)
-dim = (width, height)
-
-# resize image
-resized = cv2.resize(image, dim, interpolation = cv2.INTER_LINEAR)
-
-
-img_array1 = np.array(resized)
-st.image(img_array1,caption = 'サムネイル画像',use_column_width = True)
-
-#@title **画像全体オリジナル**
-import os
-import shutil
-from PIL import Image
-
-pic ='icon.png'#@param {type:"string"} 
-input_folder = 'inputs/whole_imgs_original'
-reset_folder(input_folder)
-im = image.open('/content/drive/MyDrive/pic/'+pic)
-im.save(input_folder+'/'+os.path.splitext(pic)[0]+'.png')
-
-w = 0.6 #@param {type:"slider", min:0.1, max:0.9, step:0.1}
-
-
-! python inference_codeformer.py --w $w\
-                                  --test_path $input_folder\
-                                  --bg_upsampler realesrgan\
-                                  --face_upsample
-
-clear_output()
-result_folder = 'results/whole_imgs_original_'+str(w)+'/final_results'
-display_result(input_folder, result_folder )
