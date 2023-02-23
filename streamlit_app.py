45ea043dbd1ef3cc41ab1c9119c8d3793f73c18e
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-02-23 10:14:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,13 @@ import tensorflow_hub as hub
 import numpy as np
 import cv2
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-st.title("Image Enhancement")
-
-st.sidebar.title("Settings")
-uploaded_file = st.sidebar.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-
-contrast_value = st.sidebar.slider("Contrast Adjustment", 0.0, 2.0, 1.0, 0.1)
-
+# Load the ESRGAN model
 hub_url = 'https://tfhub.dev/captain-pool/esrgan-tf2/1'
 sr_model = hub.load(hub_url)
 
-def upscale_image(image_path):
-    img = cv2.imread(image_path)
+# Define a function to upscale an image using the ESRGAN model
+def upscale_image(image):
+    img = cv2.imdecode(np.fromstring(image.read(), np.uint8), 1)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = tf.image.convert_image_dtype(img, tf.float32)
     img = tf.expand_dims(img, axis=0)
@@ -29,23 +22,31 @@ def upscale_image(image_path):
     output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
     return output
 
-def adjust_contrast(image, contrast):
-    brightness = 0
-    img = tf.image.adjust_contrast(image, contrast)
-    img = tf.image.adjust_brightness(img, brightness)
-    return img
-
+# Define the main function
 def main():
-    if uploaded_file is not None:
-        with st.spinner('Enhancing Image...'):
-            output_image = upscale_image(uploaded_file)
-            output_image = tf.convert_to_tensor(output_image)
-            output_image = adjust_contrast(output_image, contrast_value)
-            st.image(output_image, use_column_width=True)
-            
-            if st.button("Save Image"):
-                cv2.imwrite("enhanced_image.jpg", cv2.cvtColor(output_image.numpy(), cv2.COLOR_RGB2BGR))
-                st.success("Image saved as enhanced_image.jpg")
+    st.title("ESRGAN Image Enhancer")
 
+    # Create a file uploader
+    uploaded_file = st.file_uploader(
+        label="Upload an image",
+        type=["jpg", "jpeg", "png"],
+    )
+
+    if uploaded_file is not None:
+        # Upscale the image
+        output_image = upscale_image(uploaded_file)
+
+        # Display the enhanced image
+        st.image(output_image, caption="Enhanced Image", use_column_width=True)
+
+        # Create a button to download the enhanced image
+        st.download_button(
+            label="Download Enhanced Image",
+            data=output_image,
+            file_name="enhanced_image.png",
+            mime="image/png",
+        )
+
+# Run the app
 if __name__ == "__main__":
     main()
